17c261fda5b80c53a8af243f2a519190848fc3a3
Rainer Stropek <rainer@software-architects.at>
2020-07-30 07:29:46 +0200

    Fix typo

--- CDWPlanner/NewsletterHtmlBuilder.cs
+++ CDWPlanner/NewsletterHtmlBuilder.cs
@@ -11,7 +11,7 @@ namespace CDWPlanner
             var responseBuilder = new StringBuilder(@"<section class='main'><table width = '100%'>
                                     <tbody><tr><td>&nbsp;</td><td class='main-td' width='600'>
 			                        <h1>Hallo&nbsp;*|FNAME|*,</h1>
-			                        <p>Diesen Freitag ist wieder CoderDojo-Nachmittag und es sind viele Workshops im Angebot.Hier eine kurze <strong>Orientierungshilfe</strong>:</p>
+			                        <p>Diesen Freitag ist wieder CoderDojo-Nachmittag und es sind viele Workshops im Angebot. Hier eine kurze <strong>Orientierungshilfe</strong>:</p>
                                     ");
             foreach (var w in workshops)
             {
